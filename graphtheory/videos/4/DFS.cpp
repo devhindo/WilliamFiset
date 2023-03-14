9d58f341448424e8c429301065cc0d7c314c8f81
hindawi <ahmedhindawi2001@gmail.com>
2023-03-14 15:06:31 +0200

    fixed visited

--- graphtheory/videos/4/DFS.cpp
+++ graphtheory/videos/4/DFS.cpp
@@ -5,37 +5,38 @@ using namespace std;
 class Graph {
     int numberOfVertices;
     list<int> *adj;
-    bool *visited;
 
 public:
     Graph(int V);
     void addEdge(int src, int dest);
-    void DFS(int vertex);
+    void DFS(int vertex, bool* visited);
 };
 
 Graph::Graph(int vertices) {
     numberOfVertices = vertices;
     adj = new list<int>[numberOfVertices];
-    visited = new bool[numberOfVertices];
 }
 
 void Graph::addEdge(int src, int dest) {
     adj[src].push_front(dest);
 }
 
-void Graph::DFS(int vertex) {
+void Graph::DFS(int vertex, bool* visited) {
     visited[vertex] = true;
     list<int> adjList = adj[vertex];
     cout << vertex << " ";
     for(auto i = adjList.begin(); i != adjList.end(); i++) {
         if(!visited[*i]) {
-            DFS(*i);
+            DFS(*i, visited);
         }
     }
 }
 
 int main() {
-    Graph g(4);
+    int numberOfVertices = 4;
+    Graph g(numberOfVertices);
+    bool *visited;
+    visited = new bool[numberOfVertices];
     g.addEdge(0, 1);
     g.addEdge(0, 2);
     g.addEdge(1, 2);
@@ -43,6 +44,6 @@ int main() {
     g.addEdge(2, 3);
     g.addEdge(3, 3);
 
-    g.DFS(2);
+    g.DFS(2,visited);
     return 0;
 }
