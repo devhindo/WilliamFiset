f6138862f1e5e4ad4d2f182d92224b688e8dfc18
hindawi <ahmedhindawi2001@gmail.com>
2023-03-14 14:50:41 +0200

    added implementation of DFS algorithm

--- graphtheory/videos/4/DFS.cpp
+++ graphtheory/videos/4/DFS.cpp
@@ -1,47 +1,48 @@
 #include <iostream>
-#include <vector>
-
+#include <list>
 using namespace std;
 
 class Graph {
-private:
-    int n;
-    vector<vector<int>> adj;
-    vector<bool>visited;
+    int numberOfVertices;
+    list<int> *adj;
+    bool *visited;
 
 public:
-    Graph(int n) {
-        this->n = n;
-        adj.resize(n);
-        visited.resize(n, false);
-    }
-    void addEdge(int u, int v);
-    void DFS(int v);
-
-void addEdge(int u, int v) {
-    adj[u].push_back(v);
-    adj[v].push_back(u);
-}
-
-void DFS(int s) {
-    if(visited[s]) return;
-    visited[s] = true;
-    cout << s << " ";
-    vector<int> neighbours = adj[s];
-    for(auto v: neighbours) {
-        DFS(v);
-    }
-
-}
+    Graph(int V);
+    void addEdge(int src, int dest);
+    void DFS(int vertex);
 };
 
+Graph::Graph(int vertices) {
+    numberOfVertices = vertices;
+    adj = new list<int>[numberOfVertices];
+    visited = new bool[numberOfVertices];
+}
+
+void Graph::addEdge(int src, int dest) {
+    adj[src].push_front(dest);
+}
+
+void Graph::DFS(int vertex) {
+    visited[vertex] = true;
+    list<int> adjList = adj[vertex];
+    cout << vertex << " ";
+    for(auto i = adjList.begin(); i != adjList.end(); i++) {
+        if(!visited[*i]) {
+            DFS(*i);
+        }
+    }
+}
 
 int main() {
-    Graph g(5);
+    Graph g(4);
     g.addEdge(0, 1);
     g.addEdge(0, 2);
-    g.addEdge(1, 3);
-    g.addEdge(2, 4);
-    g.DFS(0);
+    g.addEdge(1, 2);
+    g.addEdge(2, 0);
+    g.addEdge(2, 3);
+    g.addEdge(3, 3);
+
+    g.DFS(2);
     return 0;
 }
